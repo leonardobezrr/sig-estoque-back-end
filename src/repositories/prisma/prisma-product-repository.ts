d921073de67cae9addd1bd3a57a9d2c13e243a24
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-28 13:16:12 -0300

    #11 - feat: creating repository layer for products

---
+++ src/repositories/prisma/prisma-product-repository.ts
@@ -0,0 +1,29 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { ProductRepository } from "../product-repository";
+
+export class PrismaProductRepository implements ProductRepository {
+    async create(data: Prisma.ProductCreateInput) {
+        const product = await prisma.product.create({
+            data
+        });
+
+        return product;
+    }
+
+    async findMany() {
+        return prisma.product.findMany();
+    }
+
+    async findManyByName(name: string) {
+        return prisma.product.findMany({
+            where: {
+                name
+            }
+        });
+    }
+
+    async findById(id: string) {
+        return prisma.product.findUnique({ where: { id } });
+    }
+}
---
+++ src/repositories/product-repository.ts
@@ -0,0 +1,8 @@
+import { Product, Prisma } from '@prisma/client' 
+
+export interface ProductRepository {
+    create(data: Prisma.ProductCreateInput): Promise<Product>
+    findMany(): Promise<Product[]>
+    findManyByName(name: string): Promise<Product[]>
+    findById(id: string): Promise<Product | null>
+}
