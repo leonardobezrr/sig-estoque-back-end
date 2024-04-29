f8e7b1495b5e09dcb3f85d48f37e3dd1829cc920
ricsjs <ricfilho0007@gmail.com>
2024-04-29 09:52:24 -0300

    fix: correction of necessary fields in product registration #8

--- src/services/create-product.ts
+++ src/services/create-product.ts
@@ -5,6 +5,8 @@ interface CreateProductServiceRequest {
     name: string;
     description: string;
     price: number;
+    quantity_in_stock: number,
+    batch: string
 }
 
 interface CreateProductServiceResponse {
@@ -16,12 +18,14 @@ export class CreateProductService {
     }
 
     async handle({
-        name, description, price
+        name, description, price, quantity_in_stock, batch
     }: CreateProductServiceRequest): Promise<CreateProductServiceResponse> {
         const product = await this.productRepository.create({
             name,
             description,
-            price
+            price,
+            quantity_in_stock,
+            batch
         });
 
         return {
