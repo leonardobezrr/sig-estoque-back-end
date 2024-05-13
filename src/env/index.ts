00fe1090a8dce78ce21989c15e0e520028111083
ricsjs <ricfilho0007@gmail.com>
2024-05-13 11:23:51 -0300

    config: jwt token configuration implementation #10

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
