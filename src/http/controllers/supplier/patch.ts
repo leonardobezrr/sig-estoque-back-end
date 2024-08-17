cd815cac92010cda3b5fcb1f23a70d3b1efc24b6
ricsjs <ricfilho0007@gmail.com>
2024-08-17 14:40:11 -0300

    fix: removing unnecessary validators

--- src/http/controllers/supplier/patch.ts
+++ src/http/controllers/supplier/patch.ts
@@ -1,14 +1,13 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { makePatchSupplierService } from "../../../services/factories/supplier/make-patch-supplier-service";
-import { validateCNPJ, validatePhoneNumber } from "../../../utils/validators";
 
 export async function patchSupplier(request: FastifyRequest, reply: FastifyReply) {
     const patchSupplierBodySchema = z.object({
         social_name: z.string().optional(),
         company_name: z.string().optional(),
-        phone_number: z.string().refine(validatePhoneNumber, { message: 'Número de telefone inválido' }).optional(),
-        cnpj: z.string().refine(validateCNPJ, { message: 'CNPJ inválido' }).optional()
+        phone_number: z.string().optional(),
+        cnpj: z.string().optional()
     });
 
     const { id } = request.params as { id: string };
